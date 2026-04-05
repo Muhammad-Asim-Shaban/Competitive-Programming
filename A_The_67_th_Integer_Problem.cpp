ba7086389a722c5fd145946a3ef9b89b1fa13c3f
asim443 <asimshaban43@gmail.com>
2026-04-05 12:57:28 +0500

    05/04/2026

---
+++ A_The_67_th_Integer_Problem.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        if(n==67)
+        cout<<"67"<<endl;
+        else if(n==-67)
+        cout<<"-67"<<endl;
+        else
+        cout<<n+1<<endl;
+    }
+}
